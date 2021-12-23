5926a61370a4d4b8d9bf853fc38585fcb7a62c87
Lionel Smith <smithlionel1121@gmail.com>
2021-12-23 01:31:13 +0000

    Create checkbox component

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,14 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 
+import * as colors from "../../colors";
+
 export default class CheckBox extends React.Component {
   // Create a custom checkbox component
 
   render() {
-    return <CheckboxCont></CheckboxCont>;
+    const { label } = this.props;
+    return (
+      <CheckboxLabel>
+        <CheckboxInput type="checkbox" />
+        {label}
+      </CheckboxLabel>
+    );
   }
 }
 
-const CheckboxCont = styled.div`
-  position: relative;
+const CheckboxLabel = styled.label`
+  display: grid;
+  grid-template-columns: min-content auto;
+  gap: 0.5em;
+  margin-top: 0.2em;
+`;
+
+const CheckboxInput = styled.input`
+  appearance: none;
+  font: inherit;
+  height: 1.2em;
+  width: 1.2em;
+  border: 0.1em solid currentColor;
+  border-radius: 0.2em;
+  background-color: #fff;
+
+  display: grid;
+  margin: auto 0;
+  place-content: center;
+
+  &::before {
+    content: "";
+    height: 0.7em;
+    width: 0.7em;
+    box-shadow: inset 1em 1em ${colors.primaryColor};
+    background-color: CanvasText;
+    transform: scale(0);
+    transition: 125ms transform ease-in-out;
+    transform-origin: bottom left;
+    clip-path: polygon(14% 44%, 0 65%, 50% 100%, 100% 16%, 80% 0%, 43% 62%);
+  }
+
+  &:checked::before {
+    transform: scale(1);
+  }
+
+  &:focus {
+    outline: max(1px, 0.1em) solid currentColor;
+    outline-offset: max(2px, 0.1em);
+  }
 `;
