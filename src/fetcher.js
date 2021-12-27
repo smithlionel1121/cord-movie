7e0bbd1ae658609c11e2410db8133ec5f8eb1860
Lionel Smith <smithlionel1121@gmail.com>
2021-12-27 21:31:48 +0000

    Refactor fetching functions to use URL object

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,15 +1,23 @@
 import axios from "axios";
 
 // All of your API requests should be in this file
-const baseURL = "https://api.themoviedb.org/3/";
-const API_KEY = process.env.REACT_APP_API_KEY;
+const baseURL = "https://api.themoviedb.org/";
+const apiURL = new URL("/3/", baseURL);
 
-export async function getPopularMovies() {
-  const res = await axios.get(`${baseURL}discover/movie?api_key=${API_KEY}`);
+const API_KEY = process.env.REACT_APP_API_KEY;
+apiURL.search = `api_key=${API_KEY}`;
+
+export async function getPopularMovies(params = null) {
+  const url = new URL(apiURL);
+  url.pathname += "discover/movie";
+  const res = await axios.get(url);
   return res.data;
 }
+
 export async function getMovieGenres() {
-  const res = await axios.get(`${baseURL}genre/movie/list?api_key=${API_KEY}`);
+  const url = new URL(apiURL);
+  url.pathname += "genre/movie/list";
+  const res = await axios.get(url);
   return res.data;
 }
 
