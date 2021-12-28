f608c68ba8782da6239e81223f843befe66e9d01
Lionel Smith <smithlionel1121@gmail.com>
2021-12-28 22:13:51 +0000

    Display error message for invalid year inputs

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import * as colors from "../../colors";
@@ -8,36 +8,45 @@ import CalendarIcon from "../../images/year-icon.png";
 const startYear = 1900;
 const currentYear = new Date().getFullYear();
 
-export default class SearchBar extends React.Component {
-  render() {
-    const { keyword, year, updateInput } = this.props;
-    return (
-      <>
-        <SearchBarWrapper>
-          <InputBar
-            type="search"
-            value={keyword}
-            onChange={updateInput}
-            name="keyword"
-          />
-          <SearchBarIcon src={SearchIcon} />
-        </SearchBarWrapper>
+export default function SearchBar({ keyword, year, updateInput }) {
+  const [validYear, setValidYear] = useState(true);
 
-        <SearchBarWrapper>
-          <InputBar
-            type="number"
-            placeholder="Year of release"
-            value={year || ""}
-            onChange={updateInput}
-            name="year"
-            min={startYear}
-            max={currentYear}
-          />
-          <SearchBarIcon src={CalendarIcon} />
-        </SearchBarWrapper>
-      </>
-    );
-  }
+  useEffect(() => {
+    setValidYear(!year || (startYear <= year && year <= currentYear));
+  }, [year]);
+
+  return (
+    <>
+      <SearchBarWrapper>
+        <InputBar
+          type="search"
+          value={keyword}
+          onChange={updateInput}
+          name="keyword"
+          autoComplete="off"
+        />
+        <SearchBarIcon src={SearchIcon} />
+      </SearchBarWrapper>
+
+      <SearchBarWrapper>
+        <InputBar
+          type="number"
+          placeholder="Year of release"
+          value={year || ""}
+          onChange={updateInput}
+          name="year"
+          min={startYear}
+          max={currentYear}
+        />
+        <SearchBarIcon src={CalendarIcon} />
+      </SearchBarWrapper>
+      {!validYear && (
+        <InputValidationMessage>
+          Please enter a year between {startYear} and {currentYear}
+        </InputValidationMessage>
+      )}
+    </>
+  );
 }
 
 const SearchBarWrapper = styled.div`
@@ -48,6 +57,7 @@ const InputBar = styled.input`
   border-style: none;
   border-bottom-style: solid;
   border-color: ${colors.primaryColor};
+  color: ${colors.primaryColor};
   width: 100%;
   padding: 16px 0 16px 35px;
   box-sizing: border-box;
@@ -55,7 +65,7 @@ const InputBar = styled.input`
   font-size: 1.17em;
   font-weight: bold;
 
-  ::placeholder {
+  &::placeholder {
     color: ${colors.primaryColor};
     font-weight: 300;
   }
@@ -66,4 +76,8 @@ const SearchBarIcon = styled.img.attrs({ draggable: false })`
   top: 50%;
   left: 0;
   transform: translateY(-50%);
+`;
+
+const InputValidationMessage = styled.span`
+  color: red;
 `;
